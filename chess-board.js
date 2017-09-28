7be48b73b9468766e859cfb12262d0060bf4208a
Prahalad Belavadi <prahaladbelavadi@users.noreply.github.com>
2017-09-28 14:05:20 +0530

    Create chess-board.js

---
+++ chess-board.js
@@ -0,0 +1,14 @@
+var num = 8;
+var str = "";
+for (var y = 0;y < num ;y++){
+	for (var x = 0;x < num ; x++){
+		if ((x+y)%2==0){
+		str =str+" ";
+		}
+		else{
+			str=str+"#";
+		}
+  	};
+	str = str + "/n"
+};
+console.log(str)
