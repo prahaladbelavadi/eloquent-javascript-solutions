92ae6cf2c797b55b970131e773a671e72cabd5b9
Prahalad Belavadi <prahaladbelavadi@users.noreply.github.com>
2017-11-11 17:51:01 +0530

    Update countingBs.js

--- 3.functions/countingBs.js
+++ 3.functions/countingBs.js
@@ -1,8 +1,16 @@
-function countBs(str){
-for (i in str){
-  var result = 0;
-  if (charAt(i)=='B'){
-  	result++
-  }
+function countChar(str, letter) {
+  var counted = 0;
+  for (var i = 0; i < str.length; i++)
+    if (str.charAt(i) == letter)
+      counted += 1;
+  return counted;
 }
+
+function countBs(string) {
+  return countChar(string, "B");
 }
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
