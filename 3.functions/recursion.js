be570160997d49e2a7857f89902b8e67a74c762f
Prahalad Belavadi <prahaladbelavadi@users.noreply.github.com>
2017-11-11 14:28:12 +0530

    edits
    lesser than 0 and return statements included

--- 3.functions/recursion.js
+++ 3.functions/recursion.js
@@ -1,16 +1,11 @@
-// Your code here.
 function isEven(num){
-if (num == 0){
-return 'Even'
-}else if(num ==1) {
-return 'Odd'
-}else {
-	isEven(num-2) 
+  if (num == 0){
+  return true;
+  }else if(num == 1){
+  return false;
+  }else if (num < 0){
+  return isEven(-num);
+  }else{
+  return isEven(num-2);
+  }
 }
-}
-console.log(isEven(50));
-// → true
-console.log(isEven(75));
-// → false
-console.log(isEven(-1));
-// → ??
