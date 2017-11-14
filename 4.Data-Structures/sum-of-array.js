4261ca00a7fb860fe090c67d8f80acc8d5960316
Prahalad Belavadi <prahaladbelavadi@users.noreply.github.com>
2017-11-15 03:34:03 +0530

    need to evaluate differences

--- 4.Data-Structures/sum-of-array.js
+++ 4.Data-Structures/sum-of-array.js
@@ -1,32 +1,25 @@
-// Your code here.
-function range(start, end, step){
-var arr = [];
-  if(step == null){
-    for (var i = start;i < end;i++){
-	arr.push(i)
-	return arr
-	}
-  }else if(step < 0){
-    for (var i = start;i < end;i=i-step){
-	arr.push(i)
-	return arr
-	}
+function range(start, end, step) {
+  if (step == null) step = 1;
+  var array = [];
+
+  if (step > 0) {
+    for (var i = start; i <= end; i += step)
+      array.push(i);
+  } else {
+    for (var i = start; i >= end; i += step)
+      array.push(i);
   }
-  else{
-  for (var i = start;i < end;i=i+step){
-	arr.push(i)
-	return arr
-		}
-	}
-}  
-function sum(x){
-var su = 0;
-  for (var i; i < x.length; i++){
-	su = su + i;
-	}
+  return array;
 }
 
-console.log(range(1, 10));
+function sum(array) {
+  var total = 0;
+  for (var i = 0; i < array.length; i++)
+    total += array[i];
+  return total;
+}
+
+console.log(range(1, 10))
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
