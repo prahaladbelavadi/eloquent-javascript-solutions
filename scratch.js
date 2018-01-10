1a85e161e1833d22ea25e661c3634c580afc9501
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-01-10 15:55:07 +0530

    sum function

--- scratch.js
+++ scratch.js
@@ -1,9 +1,18 @@
 var array = [];
-function range(start,end){
+function ran(start,end){
    for (i=start;i<=end;i++){
      array.push(i);
    }
    return array;
 }
+ran(5,10);
 
-range(5,10);
+var tot = 0;
+function su(arr){
+for(i in arr.length){
+      tot = tot+arr[i];
+  }
+  return tot;
+}
+
+su(ran(5,10))
