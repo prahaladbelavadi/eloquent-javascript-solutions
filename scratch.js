689fc8bedfd2b34caa6826abd422a06d8f33278c
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-01-27 15:06:34 +0530

    todolist

--- scratch.js
+++ scratch.js
@@ -1,18 +1,15 @@
-var array = [];
-function ran(start,end){
-   for (i=start;i<=end;i++){
-     array.push(i);
-   }
-   return array;
-}
-ran(5,10);
+var todoList = [];
 
-var tot = 0;
-function su(arr){
-for(i in arr.length){
-      tot = tot+arr[i];
-  }
-  return tot;
+function rememberTo(Task) {
+
+  todoList.push(task);
+
 }
 
-su(ran(5,10))
+function whatIsNext() {
+  return todoList.shift();
+}
+
+function urgentlyRememberTo(Task) {
+  todoList.unshift(task);
+}
